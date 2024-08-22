4ca7d2d02e0bae9edae7d56d7564502e59f2653a
Ruan <133697229+RuanFioriMarcelino@users.noreply.github.com>
2024-08-22 17:04:35 -0300

    readjust branches

--- Cantina/src/screen/drinks.tsx
+++
@@ -1,8 +0,0 @@
-import { SafeAreaView } from "react-native";
-import AvatarBar from "../components/avatarBar";
-
-export default function Drinks(){
-    return  <SafeAreaView className="flex-1">
-    <AvatarBar/>
-</SafeAreaView>
-}
