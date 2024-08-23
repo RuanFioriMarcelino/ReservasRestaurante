7af52ad76ebf844ae17f98acdbdb9e16bb3c2be6
RuanFioriMarcelino <ruanfiorimarcelino@gmail.com>
2024-08-22 23:46:50 -0300

    initial commit

---
+++ cantinaPC/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="bg-black_ w-full h-full flex-1 ">
+        <p className="text-orange-600">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
